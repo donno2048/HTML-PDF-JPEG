4ed269582981194d342e84770bc707914ae01e08
Elisha Hollander <just4now666666@gmail.com>
2025-01-22 09:16:53 +0200

    small refactor
    - remove body and html closing tag to make the pdf and part of jpg a part of the html so it'll be considered more valid
    - add jpg end marker to make sure the pdf isn't included
    - make the jpg a pdf comment to avoid eccidental pdf header (html won't include any)
    - move actual pdf to the end to make it arbitrarily large

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from io import BytesIO
 from struct import pack
 from PIL import Image, ImageDraw
 def text2html(text: bytes) -> bytes:
-    return b"<html><head><style>body{visibility:hidden}div{visibility:visible}</style></head><body><div>" + text + b"</div></body></html>"
+    return b"<html><head><style>body{visibility:hidden}div{visibility:visible}</style></head><body><div>" + text + b"</div>"
 def text2pdf(text: bytes, fontsize: int = 100) -> bytes:
     return b"1 0 obj << /Type /Catalog /Pages << /Type /Pages /Kids [ << /Type /Page /Contents <<>> stream\n/F1 " + str(fontsize).encode() + b" Tf (" + text + b") Tj endstream >> ] /Count 1 >> >> endobj trailer << /Root 1 0 R >>"
 def text2jpeg(text: bytes, width: int = 100, height: int = 100, x: int = 0, y: int = 0) -> bytes:
@@ -13,7 +13,7 @@ def text2jpeg(text: bytes, width: int = 100, height: int = 100, x: int = 0, y: i
     return out.getvalue()
 def embed(html: bytes, pdf: bytes, jpeg: bytes) -> bytes:
     offset = jpeg[jpeg.find(b"F"):].find(0xFF) + jpeg.find(b"F")
-    return jpeg[:offset] + b"\xff\xfe" + pack(">h", len(pdf) + len(html) + 11) + html + b"<!--%PDF\n" + pdf + jpeg[offset:]
+    return jpeg[:offset] + b"\xff\xfe" + pack(">h", len(html) + 29) + html + b"%PDF\n999 0 obj\n<<>>\nstream\n" + jpeg[offset:] + b"\xff\xd9\nendstream\nendobj\n" + pdf
 def main(html: bytes, pdf: bytes, jpeg: bytes, outfile: str) -> bytes:
     open(outfile, "wb").write(embed(text2html(html), text2pdf(pdf), text2jpeg(jpeg)))
 text = """
